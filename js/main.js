cc26f5c4008c30211666eae49f530970a3f1887f
giuliaruggeri <giuliaruggeri1210@gmail.com>
2023-11-10 15:30:06 +0100

    Prima milestone

---
+++ js/main.js
@@ -0,0 +1,48 @@
+	    let teamList=[
+
+    {
+        'photo':"wayne-barnett-founder-ceo.jpg",
+        'name':"Wayne Barnett",
+        'job':"Founder & CEO",	
+    },
+
+    {
+        'photo':"angela-caroll-chief-editor.jpg",
+        'name': "Angela Caroll",
+        'job': "Chief Editor",
+    },
+
+    {
+        'photo':" walter-gordon-office-manager.jpg",
+        'name': "Walter Gordon",
+        'job':"Office Manager",
+    },
+
+    {
+        'photo':"angela-lopez-social-media-manager.jpg",
+        'name': "Angela Lopez",
+        'job':"Social Media Manager",
+    },
+
+    {
+        'photo':"scott-estrada-developer.jpg",
+        'name': "Scott Estrada",
+        'job':"Developer",
+    },
+
+    {
+        'photo':"barbara-ramos-graphic-designer.jpg",
+        'name': "Barbara Ramos",
+        'job':"Graphic Designer",
+    },
+
+
+
+]
+
+for(i=0; i<teamList.length; i++){
+
+    console.log(teamList[i].photo);
+    console.log(teamList[i].name);
+    console.log(teamList[i].job);
+}
